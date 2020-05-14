a46b738ec6656c84a87de4e93e05f14c1424aa26
rastapf <rupertstapf@hotmail.com>
2020-05-14 16:28:21 -0300

    Todo list completed

---
+++ js/todo.js
@@ -0,0 +1,27 @@
+const taskInput = document.querySelector('#novaTarefa')
+    const addButton = document.querySelector('#add')
+    const boardDiv = document.querySelector('#board')
+
+    addButton.addEventListener('click', addTask)
+    taskInput.addEventListener('keyup', (event) => {
+        if(event.keyCode === 13) {
+            addTask()
+        }
+    })
+
+    boardDiv.addEventListener('click', (event) => {
+        if(event.target.id != "board"){
+            event.target.closest('.tarefa').remove()
+        }
+    })
+
+    function addTask() {
+        text = taskInput.value
+        boardDiv.innerHTML += `
+            <div class="tarefa">
+                <div class="col-md-8">${text}</div>
+                <div class="col-md-2"><img class="icon" src="img/check.png"></div>
+            </div>
+        `
+        taskInput.value = ""
+    }
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Digital House: Aula 14 Maio 2020
+## Criar uma lista de tarefas utilizando apenas HTML/CSS/JS (Front) 
+
+- Nova tarefa deve ser criada ao digitar o texto no input e pressionar "enter"
+- Nova tarefa pode ser adicionada utilizando o botão "+"
+- Input deve ser limpo após inclusão da nova tarefa
+- Tarefa deve ser excluída ao clicar sobre o card

2ed64be3ae7957696dc33ac03b7dfc2875eb3c63
rastapf <rupertstapf@hotmail.com>
2020-05-15 05:25:55 -0300

    Included support for localStorage

--- js/todo.js
+++ js/todo.js
@@ -1,6 +1,16 @@
-const taskInput = document.querySelector('#novaTarefa')
+window.addEventListener('load', function () {
+    const taskInput = document.querySelector('#novaTarefa')
     const addButton = document.querySelector('#add')
     const boardDiv = document.querySelector('#board')
+    let taskList = []
+
+    if(typeof localStorage.taskList !== 'undefined') {
+        taskList = JSON.parse(localStorage.taskList)
+    }
+
+    taskList.forEach(task => {
+        addTask(task, false)
+    });
 
     addButton.addEventListener('click', addTask)
     taskInput.addEventListener('keyup', (event) => {
@@ -11,19 +21,47 @@ const taskInput = document.querySelector('#novaTarefa')
 
     boardDiv.addEventListener('click', (event) => {
         if(event.target.id != "board"){
-            event.target.closest('.tarefa').remove()
+            const targetClasses = event.target.classList
+            for(let i = 0; i < targetClasses.length; i++) {
+                let taskValue = ""
+                if(targetClasses[i] == "checkmark") {
+                    taskValue = event.target.previousElementSibling.innerHTML                   
+                } else if(targetClasses[i] == "icon") {
+                    taskValue = event.target.parentElement.previousElementSibling.innerHTML
+                }
+
+                if(taskValue != "") {                                
+                    for(let j = 0; j < taskList.length; j++) {
+                        if(taskList[j] == taskValue) {
+                            taskList.splice(j, 1)
+                            localStorage.taskList = JSON.stringify(taskList)
+                            event.target.closest('.tarefa').remove()
+                            return
+                        }
+                    }
+                }
+            }
         }
     })
 
-    function addTask() {
-        text = taskInput.value
+    function addTask(text = "", save = true) {
+        if(text == "") {
+            text = taskInput.value
+        }
+
         if(text.trim() != "") {
             boardDiv.innerHTML += `
                 <div class="tarefa">
-                    <div class="col-md-8">${text}</div>
-                    <div class="col-md-2"><img class="icon" src="img/check.png"></div>
+                    <div class="col-md-8 taskValue">${text}</div>
+                    <div class="col-md-2 checkmark"><img class="icon" src="img/check.png"></div>
                 </div>
             `
+            if(save === false) {
+                return
+            }
+
+            taskList.push(text)
+            localStorage.taskList = JSON.stringify(taskList)
             taskInput.value = ""
             taskInput.style.borderColor = 'black'
             return;
@@ -33,3 +71,4 @@ const taskInput = document.querySelector('#novaTarefa')
             return;
         }
     }
+})
